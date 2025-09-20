ee817e8511446a41d10788b5d81869ad300007a0
Anuj Rawat <rawatanuj0063@gmail.com>
2025-09-20 12:52:01 +0530

    Separate into smaller components

---
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -0,0 +1,30 @@
+import { DeliveryDate } from "./DeliveryDate";
+import { CartItemDetails } from "./CartItemDetails";
+import { DeliveryOptions } from "./DeliveryOptions";
+
+export function OrderSummary({ cart, deliveryOptions }) {
+  return (
+    <div className="order-summary">
+      {deliveryOptions.length > 0 && cart.map((cartItem) => {
+
+        return (
+          <div key={cartItem.productId} className="cart-item-container">
+            <DeliveryDate
+              deliveryOptions={deliveryOptions}
+              cartItem={cartItem} 
+            />
+
+            <div className="cart-item-details-grid">
+              <CartItemDetails cartItem={cartItem} />
+
+              <DeliveryOptions
+                deliveryOptions={deliveryOptions}
+                cartItem={cartItem} 
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
