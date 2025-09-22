359860cec1d4f08b4743d84a1eaa7f2206cffa66
Anuj Rawat <rawatanuj0063@gmail.com>
2025-09-22 16:05:26 +0530

    Use beforeEach to remove duplicaton

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -6,27 +6,27 @@ import { Product } from "./Product";
 
 vi.mock('axios');
 
-let product;
-
-let loadCart;
-
-beforeEach(() => {
-  product = {
-    "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
-    "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    "rating": {
-      "stars": 4.5,
-      "count": 87
-    },
-    "priceCents": 1090,
-    "keywords": ["socks", "sports", "apparel"]
-  }
-
-  loadCart = vi.fn();
-})
-
 describe('Product component', () => {
+
+  let product;
+  let loadCart;
+
+  beforeEach(() => {
+    product = {
+      "id": "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      "image": "images/products/athletic-cotton-socks-6-pairs.jpg",
+      "name": "Black and Gray Athletic Cotton Socks - 6 Pairs",
+      "rating": {
+        "stars": 4.5,
+        "count": 87
+      },
+      "priceCents": 1090,
+      "keywords": ["socks", "sports", "apparel"]
+    }
+
+    loadCart = vi.fn();
+  });
+
   it('displays the product details correctly', () => {
 
     render(<Product product={product} loadCart={loadCart} />)
