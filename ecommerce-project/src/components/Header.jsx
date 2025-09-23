402ecf3cac4b025547cb7163b73a707df4fc9911
Anuj Rawat <rawatanuj0063@gmail.com>
2025-09-23 16:24:04 +0530

    Create test for Header component

--- ecommerce-project/src/components/Header.jsx
+++ ecommerce-project/src/components/Header.jsx
@@ -35,28 +35,35 @@ export function Header({cart}) {
         <div className="left-section">
           <NavLink to="/" className="header-link">
             <img className="logo"
+              data-testid="header-logo"
               src={LogoWhite} />
             <img className="mobile-logo"
+              data-testid="header-mobile-logo"
               src={MobileLogoWhite} />
           </NavLink>
         </div>
 
         <div className="middle-section">
           <input className="search-bar" type="text" placeholder="Search"
+            data-testid="header-search-bar"
             value={search} onChange={updateSearchInput} />
 
-          <button className="search-button" onClick={searchProducts}>
+          <button className="search-button" 
+            data-testid="header-search-button" 
+            onClick={searchProducts}>
             <img className="search-icon" src={SearchIcon} />
           </button>
         </div>
 
         <div className="right-section">
-          <NavLink className="orders-link header-link" to="/orders">
+          <NavLink className="orders-link header-link" to="/orders"
+            data-testid="header-orders-link">
 
             <span className="orders-text">Orders</span>
           </NavLink>
 
-          <NavLink className="cart-link header-link" to="/checkout">
+          <NavLink className="cart-link header-link" to="/checkout"
+            data-testid="header-cart-link">
             <img className="cart-icon" src={CartIcon} />
             <div className="cart-quantity">{totalQuantity}</div>
             <div className="cart-text">Cart</div>
