6f2f4e71c907162cba969360fd02ee2a45ead34b
Anuj Rawat <rawatanuj0063@gmail.com>
2025-09-23 17:14:43 +0530

    Create tests for OrderSummary component

--- ecommerce-project/src/pages/checkout/CartItemDetails.jsx
+++ ecommerce-project/src/pages/checkout/CartItemDetails.jsx
@@ -41,17 +41,20 @@ export function CartItemDetails({ cartItem, loadCart }) {
   return (
     <>
       <img className="product-image"
-        src={cartItem.product.image} />
+        src={cartItem.product.image} 
+        data-testid="cart-item-image"/>
 
       <div className="cart-item-details">
-        <div className="product-name">
+        <div className="product-name"
+          data-testid="cart-item-name">
           {cartItem.product.name}
         </div>
-        <div className="product-price">
+        <div className="product-price"
+          data-testid="cart-item-price">
           {formatMoney(cartItem.product.priceCents)}
         </div>
         <div className="product-quantity">
-          <span>
+          <span data-testid="cart-item-quantity">
             Quantity: {isUpdatingQuantity
               ? <input type="text" className="quantity-textbox" 
                   value={quantity} onChange={updateQuantityInput}
@@ -64,7 +67,9 @@ export function CartItemDetails({ cartItem, loadCart }) {
             onClick={updateQuantity}>
             Update
           </span>
-          <span className="delete-quantity-link link-primary" onClick={deleteCartItem}>
+          <span className="delete-quantity-link link-primary"
+            data-testid="cart-item-delete-quantity-link"  
+            onClick={deleteCartItem}>
             Delete
           </span>
         </div>
