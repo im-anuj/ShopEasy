5cf3da2b3179cac4c05ab5e4cddaa4bb941717d5
Anuj Rawat <rawatanuj0063@gmail.com>
2025-09-23 16:31:31 +0530

    Create test for CheckoutPage component

--- ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
+++ ecommerce-project/src/pages/checkout/CheckoutHeader.jsx
@@ -8,7 +8,8 @@ export function CheckoutHeader(){
   return(
     <>
       <div className="checkout-header">
-        <div className="header-content">
+        <div className="header-content"
+          data-testid="checkout-header">
           <div className="checkout-header-left-section">
             <Link to="/">
               <img className="logo" src={Logo} />
